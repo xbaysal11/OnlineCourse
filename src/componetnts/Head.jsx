932753fc37348ca9f08cf567daabda9c62b7d043
NasipCOM <salmahunov@mail.ru>
2019-02-26 22:21:48 +0600

    Bg image

--- src/componetnts/Head.jsx
+++ src/componetnts/Head.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { Menu, Icon } from 'antd';
+import { Row, Col } from 'antd';
+import { Button } from 'antd';
+import styled from 'styled-components';
+import bgImg from '../img/rawpixel-798163-unsplash.jpg'
+import '../css/index.css';
+
 
 class Head extends Component {
     render() {
         return (
-            <div>
+            <BgImg>
+                <img src={bgImg} className="image"></img>
+                <Col span={18} offset={3}>
+                    <MyInput placeholder = "Search Catalog"></MyInput>
+
+                </Col>
                 
-                {/*  <Col span={18} offset={3}>
-                 
-                 
-                 </Col> */}
-            </div>
+            </BgImg>
         );
     }
 }
 
 export default Head;
+
+
+const BgImg = styled.div`
+ 
+`
+const MyInput = styled.input`
+background-color:rgb(49, 49, 49);
+  border: 1px solid rgb(49, 49, 49);
+  border-radius: 5px;
+  height:2rem;`
