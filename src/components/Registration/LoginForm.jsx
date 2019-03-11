f5ba33198895628cd954817912b3198295496905
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-03-11 20:53:39 +0600

    MODIFIED LoginForm via formik

--- src/components/Registration/LoginForm.jsx
+++ src/components/Registration/LoginForm.jsx
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
-import formik from "formik";
+import { Formik } from "formik";
+import { Input as InputAntd, Button } from "antd";
 
 class LoginForm extends Component {
     render() {
-        return <div />;
+        return (
+            <Formik>
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting
+                    /* and other goodies */
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <InputAntd
+                            placeholder="Basic usage"
+                            type="email"
+                            name="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                        />
+
+                        {errors.email && touched.email && errors.email}
+                        <InputAntd
+                            type="password"
+                            name="password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                        />
+                        {errors.password && touched.password && errors.password}
+                        <Button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </Button>
+                    </form>
+                )}
+            </Formik>
+        );
     }
 }
 export default LoginForm;
