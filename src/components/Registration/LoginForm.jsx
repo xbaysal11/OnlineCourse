fc3ff1b6ae6c66a2253ff8ace8fa897c24994894
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-03-15 16:50:31 +0600

    UPDATED LoginForm style

--- src/components/Registration/LoginForm.jsx
+++ src/components/Registration/LoginForm.jsx
@@ -1,47 +1,114 @@
 import React, { Component } from "react";
 import { Formik } from "formik";
-import { Input as InputAntd, Button } from "antd";
+import styled from "styled-components";
+
+import {
+    Form,
+    Icon,
+    Input as InputAntd,
+    Button as ButtonAntd,
+    Checkbox
+} from "antd";
 
 class LoginForm extends Component {
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log("Received values of form: ", values);
+            }
+        });
+    };
     render() {
+        const { getFieldDecorator } = this.props.form;
+        const { name } = this.props;
+
         return (
             <Formik>
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting
-                    /* and other goodies */
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <InputAntd
-                            placeholder="Basic usage"
-                            type="email"
-                            name="email"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.email}
-                        />
-
-                        {errors.email && touched.email && errors.email}
-                        <InputAntd
-                            type="password"
-                            name="password"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.password}
-                        />
-                        {errors.password && touched.password && errors.password}
-                        <Button type="submit" disabled={isSubmitting}>
-                            Submit
-                        </Button>
-                    </form>
-                )}
+                <Wrapper>
+                    <h1>Log In</h1>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator("userName", {
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: "Please input your username!"
+                                    }
+                                ]
+                            })(
+                                <Input
+                                    prefix={
+                                        <Icon
+                                            type="user"
+                                            style={{ color: "rgba(0,0,0,.25)" }}
+                                        />
+                                    }
+                                    placeholder="Username"
+                                />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator("password", {
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: "Please input your Password!"
+                                    }
+                                ]
+                            })(
+                                <Input
+                                    prefix={
+                                        <Icon
+                                            type="lock"
+                                            style={{ color: "rgba(0,0,0,.25)" }}
+                                        />
+                                    }
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator("remember", {
+                                valuePropName: "checked",
+                                initialValue: true
+                            })(<Checkbox>Remember me</Checkbox>)}
+                            <a className="login-form-forgot" href="">
+                                Forgot password
+                            </a>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                className="login-form-button"
+                            >
+                                Log in
+                            </Button>
+                            Or <a href="">register now!</a>
+                        </Form.Item>
+                    </Form>
+                </Wrapper>
             </Formik>
         );
     }
 }
-export default LoginForm;
+const WrappedLoginForm = Form.create({ name: "login" })(LoginForm);
+
+export default WrappedLoginForm;
+
+const Button = styled(ButtonAntd)`
+    width: 100%;
+`;
+
+const Wrapper = styled.div`
+    text-align: center;
+`;
+
+const Input = styled(InputAntd)`
+    max-width: 300px;
+    padding: 0 auto;
+`;
+
+const a = styled.a`
+    float: right;
+`;
