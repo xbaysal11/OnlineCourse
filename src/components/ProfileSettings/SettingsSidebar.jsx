78ada4705419ce27662b434e6ba373a9eb638f3a
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-04-03 16:51:50 +0600

    FIXED bug with settings side bar collapsibility

--- src/components/ProfileSettings/SettingsSidebar.jsx
+++ src/components/ProfileSettings/SettingsSidebar.jsx
@@ -37,7 +37,7 @@ class SettingsSidebar extends Component {
                         defaultSelectedKeys={["1"]}
                         defaultOpenKeys={["sub1"]}
                         mode="inline"
-                        toggle={this.props.onToggle}
+                        toggle={this.onToggle}
                         collapsed={this.state.collapsed}
                     >
                         <Menu.Item key="1">
