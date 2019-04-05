ee1fb146884863c80f9a3345e89b9091d7cd09bd
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-04-05 19:43:44 +0600

    ADDED account setings page

---
+++ src/components/ProfileSettings/AccountSettings.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Input, Row as RRow, Col, Button } from "antd";
+
+import styled from "styled-components";
+class ProfileInfo extends Component {
+    render() {
+        return (
+            <Wrapper>
+                <Row>
+                    <Col span={4}>
+                        <label htmlFor="">E-mail</label>
+                    </Col>
+                    <Col span={20}>
+                        <Input placeholder=" " value="123@mail.com" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={4}>
+                        <label htmlFor="">Password</label>
+                    </Col>
+                    <Col span={20}>
+                        <Input placeholder="New Password" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col offset={4}>
+                        <Input placeholder="Confirm Password " />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col offset={4}>
+                        <Input placeholder="Old Password " />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col offset={20}>
+                        <Button type="submit">Change!</Button>
+                    </Col>
+                </Row>
+            </Wrapper>
+        );
+    }
+}
+
+export default ProfileInfo;
+const Wrapper = styled.form`
+    background: #fff;
+    padding: 2em;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.15);
+    margin: 24px 12px 52px 12px;
+    min-height: 50%;
+`;
+const Row = styled(RRow)`
+    margin: 1em 0;
+`;
