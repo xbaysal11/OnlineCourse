acda48e4f6d0bf69da7c096b8e58d15ef63fe55e
NasipCOM <salmahunov@mail.ru>
2019-04-15 23:10:38 +0600

    Hover transform var

--- src/config/var.js
+++ src/config/var.js
@@ -10,8 +10,8 @@ export const shadows = {
 
     transition: ("transform 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s",
     "box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s"),
-
+/* 
     hover: ("transform: translateY(-0.25rem);",
    " box-shadow: rgba(46, 41, 51, 0.08) 0px 4px 8px",
-        "rgba(71, 63, 79, 0.16) 0px 8px 16px")
-}
+        "rgba(71, 63, 79, 0.16) 0px 8px 16px") */
+};
